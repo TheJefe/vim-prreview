6d7855e20ed1221de6ae4658caf8bce354bbd3b7
Jeff Koenig <jkoenig311@gmail.com>
2015-03-28 11:56:31 -0400

    updated tests to reflect the change in api call to list pull requests

--- test/pr_test.rb
+++ test/pr_test.rb
@@ -1,10 +1,3 @@
-#gem 'minitest'
-#gem 'vcr'
-#$: << 'lib'
-#require 'pr_review'
-#require 'minitest/autorun'
-## This stuff could be moved to a test_helper class, but for now, that is more then we need
-
 require 'test_helper'
 
 class PrReviewTest < UnitTest
@@ -12,32 +5,33 @@ class PrReviewTest < UnitTest
   describe 'pr review' do
 
     before do
-      repos = ['thinkthroughmath/mathjs-rest','jnicklas/capybara','rspec/rspec-rails']
-      filter = {'state'=>'open', 'sort'=>'updated', 'direction'=>'asc'}
+      repos = ['thinkthroughmath/mathjs-rest','neovim/neovim','jnicklas/capybara']
+      query = 'state:open type:pr'
+      options = {'sort'=>'updated', 'order'=>'asc', 'direction'=>'asc'}
 
       VCR.use_cassette("pull_requests") do
-        PrReview.print_pull_requests repos, filter
+        PrReview.print_pull_requests repos, query, options
       end
     end
 
     it 'gets a list of pull requests' do
-      assert_equal 57, VIM::Buffer.output.count
+      assert_equal 43, VIM::Buffer.output.count
     end
 
     it 'shows date' do
-      assert VIM::Buffer.output.first.include? '2015-01-23 20:58:38'
+      assert VIM::Buffer.output.first.include? '2015-01-26 19:19:00'
     end
 
     it 'shows assignee' do
-      assert VIM::Buffer.output.last.include? 'ninjapanzer'
+      assert VIM::Buffer.output[4].include? 'justinmk'
     end
 
     it 'shows title' do
-      assert VIM::Buffer.output.first.include? 'If a view example\'s description contains no path elements, then do not attempt to load a helper based on the example description.'
+      assert VIM::Buffer.output.first.include? 'Add :element option'
     end
 
-    it 'shows title' do
-      assert VIM::Buffer.output.first.include? 'rspec-rails'
+    it 'shows repo name' do
+      assert VIM::Buffer.output.first.include? 'capybara'
     end
 
   end
